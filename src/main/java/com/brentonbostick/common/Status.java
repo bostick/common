323a959167c2f9095329c1cf519daebee2a6ab55
Brenton Bostick <bostick@gmail.com>
2024-10-03 11:55:44 -0400

    add java

---
+++ src/main/java/com/brentonbostick/common/Status.java
@@ -0,0 +1,48 @@
+// Copyright (C) 2024 by Brenton Bostick
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial
+// portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+// OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+package com.brentonbostick.common;
+
+public enum Status {
+    OK(0),
+    ERR(-1),
+    CONTINUE(1);
+
+    private final int id;
+
+    Status(int id) {
+        this.id = id;
+    }
+
+    public static Status fromInt(int id) {
+        switch (id) {
+            case 0:
+                return OK;
+            case -1:
+                return ERR;
+            case 1:
+                return CONTINUE;
+            default:
+                throw new RuntimeException("Unhandled value: " + id);
+        }
+    }
+
+    public int toInt() {
+        return id;
+    }
+}
