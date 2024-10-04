0a0bd594d348bc6ce7f67ad47d9a4bb385db4e02
Brenton Bostick <bostick@gmail.com>
2024-10-04 15:44:19 -0400

    add InputStreamUtils.java

---
+++ src/test/java/com/brentonbostick/common/InputStreamUtilsTest.java
@@ -0,0 +1,41 @@
+package com.brentonbostick.common;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import android.annotation.SuppressLint;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+@RunWith(JUnit4.class)
+public class InputStreamUtilsTest {
+
+    public InputStreamUtilsTest() {
+    }
+
+    @Test
+    public void testLongToBytes() {
+
+        InputStream is = new ByteArrayInputStream("123".getBytes(StandardCharsets.UTF_8));
+
+        byte[] buf = new byte[3];
+
+        try {
+
+            int read = InputStreamUtils.readInputStreamIntoBuffer(is, buf);
+
+            assertThat(read).isEqualTo(3);
+
+            assertThat(buf).isEqualTo(new byte[]{ '1', '2', '3' });
+
+        } catch (IOException e) {
+            assertThat(true).isFalse();
+        }
+    }
+}
