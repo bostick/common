e23029380e2a4752030ae8ed0a3659c32780e384
Brenton Bostick <bostick@gmail.com>
2024-10-10 08:16:33 -0400

    add JSONObject class

--- src/main/cpp/include/common/jnicache.h
+++ src/main/cpp/include/common/jnicache.h
@@ -65,6 +65,7 @@ extern jclass Status_class;
 
 #if IS_PLATFORM_ANDROID
 extern jclass Build_class;
+extern jclass JSONObject_class;
 #endif // IS_PLATFORM_ANDROID
 
 //
@@ -82,6 +83,8 @@ extern jfieldID Build_MODEL_field;
 // Instance methods
 //
 extern jmethodID File_getAbsolutePath_method;
+extern jmethodID JSONObject_has_method;
+extern jmethodID JSONObject_getJSONArray_method;
 
 //
 // Static methods
