54a9ccfb61855a86913d81729d0e40792a67267f
Brenton Bostick <bostick@gmail.com>
2025-10-16 11:10:01 -0400

    add random.cpp

---
+++ src/main/cpp/include/common/random.h
@@ -0,0 +1,29 @@
+
+#pragma once
+
+#include <cstdint>
+
+
+int NextRandomInteger();
+
+int NextRandomIntegerInRangeInclusive(int lo, int hi);
+
+float NextRandomFloatInRangeInclusive(float lo, float hi);
+
+void SeedRandom(uint32_t s);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/cpp/lib/random.cpp
@@ -0,0 +1,49 @@
+
+#include "common/random.h"
+
+#include <random>
+
+
+static std::mt19937 gen;
+static std::uniform_int_distribution<int> dis;
+
+
+int NextRandomInteger() {
+    return dis(gen);
+}
+
+int NextRandomIntegerInRangeInclusive(int lo, int hi) {
+    return lo + (NextRandomInteger() % (hi - lo + 1));
+}
+
+
+void SeedRandom(uint32_t s) {
+    gen.seed(s);
+}
+
+
+#define RAND_LIMIT 32767
+
+// Random floating point number in range [lo, hi]
+float NextRandomFloatInRangeInclusive(float lo, float hi) {
+    auto r = (float)( NextRandomInteger() & ( RAND_LIMIT ) );
+    r /= RAND_LIMIT;
+    r = (( hi - lo ) * r) + lo;
+    return r;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
