f4b714d617deaaf8f9c5e60227d69351d1e3b503
Brenton Bostick <bostick@gmail.com>
2026-05-22 20:13:53 -0400

    clang-tidy

--- src/main/cpp/lib/random.cpp
+++ src/main/cpp/lib/random.cpp
@@ -21,7 +21,7 @@
 #include <random>
 
 
-static std::mt19937 gen; // NOLINT(bugprone-random-generator-seed)
+static std::mt19937 gen; // NOLINT(bugprone-random-generator-seed, cert-msc51-cpp)
 static std::uniform_int_distribution<int> dis;
 
 
